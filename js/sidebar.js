ff35ba65903da6dd42de79f71a3f3e24a67a22a7
Judy Effendi <effendi1@msu.edu>
2024-06-07 11:59:35 -0400

    fix sidebar and screen

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,9 +1,7 @@
 function openNav() {
     document.getElementById("sidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav() {
     document.getElementById("sidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
 }
