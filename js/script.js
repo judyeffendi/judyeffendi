e21250aa6a840a7fb0da92ec84be636bd70df705
Judy Effendi <effendi1@msu.edu>
2024-06-11 16:52:49 -0400

    fixes

--- js/script.js
+++ js/script.js
@@ -1,31 +1,60 @@
-const text = "• Life-Long Learner •Traveler •Developer";
-const element = document.getElementById('typing-text');
+class Typewriter {
+    constructor(elementId, text) {
+        this.element = document.getElementById(elementId);
+        this.text = text;
+        this.i = 0;
+    }
 
-function typeWriter(text, i, callback) {
-    if (i < text.length) {
-        element.innerHTML += text.charAt(i);
-        i++;
-        setTimeout(function() {
-            typeWriter(text, i, callback);
-        }, 100); 
-    } else {
-        if (typeof callback === 'function') {
-            callback();
+    typeWriter() {
+        if (this.i < this.text.length) {
+            this.element.innerHTML += this.text.charAt(this.i);
+            this.i++;
+            setTimeout(() => {
+                this.typeWriter();
+            }, 100);
         }
     }
 }
 
+class WorkoutDescription {
+    constructor() {
+        this.descriptions = {
+            'strength-1': '3 squats',
+            'strength-2': '5 push-ups',
+            'strength-3': '10 lunges',
+            'cardio-1': '5 minutes of running',
+            'cardio-2': '10 minutes of cycling',
+            'cardio-3': '15 minutes of jumping rope',
+        };
+
+        document.getElementById('strength-workout').addEventListener('change', (event) => this.displayDescription('strength'));
+        document.getElementById('cardio-workout').addEventListener('change', (event) => this.displayDescription('cardio'));
+    }
+
+    displayDescription(type) {
+        const selectElement = document.getElementById(type + '-workout');
+        const selectedValue = selectElement.value;
+        const descriptionText = this.descriptions[selectedValue] || 'Please select a workout to see the description.';
+        
+        // Check if description element already exists, if yes, update the text, else create new element
+        let descriptionElement = document.getElementById('description-text');
+        if (!descriptionElement) {
+            descriptionElement = document.createElement('p');
+            descriptionElement.id = 'description-text';
+            descriptionElement.classList.add('description-text', 'text-black');
+            document.querySelector('.container').appendChild(descriptionElement);
+        }
+
+        descriptionElement.innerText = descriptionText;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-    typeWriter(text, 0);
+    const workoutDescription = new WorkoutDescription();
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const images = document.querySelectorAll('.collage-container img');
-    
-    images.forEach(image => {
-        image.addEventListener('click', () => {
-            console.log(`Image ${image.alt} clicked`);
-            // Here you can add functionality to display the image in a modal, for example.
-        });
-    });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const typewriter = new Typewriter("typing-text", "• Life-Long Learner •Traveler •Developer");
+    typewriter.typeWriter();
 });
